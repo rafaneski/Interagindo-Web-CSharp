34b1aff6e277ad2eab07d58fbd5e72394e44d2f7
Rafael Jageneski <rafa.edjnonato@gmail.com>
2022-07-24 17:16:15 -0300

    Create HTTP

---
+++ HTTP/HTTP/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+using System.Net;
+using Newtonsoft.Json;
+
+namespace HTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reqlist();
+            ReqUnica();
+            Console.ReadLine();
+        }
+
+        static void ReqList() //Requisicao em lista
+        {
+            //Requisicao
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+            requisicao.Method = "GET";
+
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                //Console.WriteLine(dados.ToString());
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                foreach (Tarefa tarefa in tarefas)
+                {
+                    tarefa.Exibir();
+                }
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+
+        static void ReqUnica() //Requisicao unica
+        {
+            //Requisicao
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/5"); //Passe o id especifico que vc quer
+            requisicao.Method = "GET";
+
+            using (var resposta = requisicao.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                //Console.WriteLine(dados.ToString());
+                Tarefa tarefa = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+
+                tarefa.Exibir();
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+    }
+}
---
+++ HTTP/HTTP/Tarefa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    internal class Tarefa
+    {
+        //Baseando-se em: 
+        //"userId": 1,
+        //"id": 1,
+        //"title": "delectus aut autem",
+        //"completed": false
+
+        //Deve ter o mesmo tipo e nome:
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Exibir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"User id: {userId}");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Finalizou?: {completed}");
+            Console.WriteLine("");
+            Console.WriteLine("========================");
+        }
+    }
+}
